a43552d93e50bda8bb7305739e9e42d0d1c9f36a
Taha <137491279+Tahahokmabadi@users.noreply.github.com>
2024-08-24 02:36:52 +0330

    Update 02_Number_Guess.py

--- Assignments/Assignment_01/02_Number_Guess.py
+++ Assignments/Assignment_01/02_Number_Guess.py
@@ -1,1 +1,28 @@
-#
+import random
+
+number = random.randint(1, 100)
+min_value = 1
+max_value = 100
+
+while True:
+    guess = input(f"Enter a number between {min_value} and {max_value}:\t")
+        
+    if not guess.isdigit():
+        print("Enter numbers only.")
+        continue
+        
+    guess = int(guess)
+        
+    if guess < min_value or guess > max_value:
+        print(f"You should Enter a number between {min_value} and {max_value}.")
+        continue
+    
+    if guess == number:
+        print("You've guessed correctly!")
+        break
+    elif guess > number:
+        print("Guess a lower number!")
+        max_value = guess - 1
+    else:
+        print("Guess a higher number!")
+        min_value = guess + 1
