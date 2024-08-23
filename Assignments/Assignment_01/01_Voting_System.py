85adc0ecc02f73ed14e1f0d16cd310fbd6499e2c
Taha <137491279+Tahahokmabadi@users.noreply.github.com>
2024-08-24 01:30:43 +0330

    Update 01_Voting_System.py

--- Assignments/Assignment_01/01_Voting_System.py
+++ Assignments/Assignment_01/01_Voting_System.py
@@ -1,1 +1,20 @@
-#
+try:
+    age = int(input("Enter your age:\t"))
+    if age <= 0:
+        print("Are you sure about that?!")
+    elif age >= 18:
+        print("You can vote.")
+    else:
+        print("You can't vote.")
+        
+except ValueError:
+    print("Non-numeric inputs are not accepted!")
+    
+except KeyboardInterrupt:
+    print("Input interrupted!")
+    
+except BaseException as e:
+    print(f"An error occurred:\n{e}")
+
+finally:
+  print("Anyway, support the country!!!")
