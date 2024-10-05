9c6533366bde655feef53c82232c3858271ce816
Tahahokmabadi <Tahahokmabadi27@gmail.com>
2024-10-05 15:19:34 +0330

    Add assignment 03 - Exercise 08

--- Assignments/Assignment_03/08 - Tuple Lists.py
+++ Assignments/Assignment_03/08 - Tuple Lists.py
@@ -1,1 +1,24 @@
+while True:
+    try:
+        number_of_tuple = int(input("Enter the number of tuples: "))
+        break
+    except ValueError:
+        print("Only numeric values are acceptable. Try again...")
 
+
+list_of_tuples = []
+
+
+for i in range (number_of_tuple):
+    while True:
+        try:
+            tuples = input(f"Enter the values for the #{i + 1} tuple seperated by a space (a b): ")
+            a, b = map(int, tuples.split())
+            list_of_tuples.append((a, b))
+            break
+        except ValueError:
+            print("Only numeric values are acceptable. Try again...")
+            continue
+         
+reault = sorted(list_of_tuples, key=lambda x: x [1])
+print(reault)
