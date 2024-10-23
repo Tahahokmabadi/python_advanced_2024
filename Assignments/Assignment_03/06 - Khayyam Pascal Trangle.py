60742c8e5c2c76860bdea0bc28fc35554e8b6857
Tahahokmabadi <Tahahokmabadi27@gmail.com>
2024-10-23 14:38:59 +0330

    Update Khayyam Pascal Triangle

--- Assignments/Assignment_03/06 - Khayyam Pascal Trangle.py
+++ Assignments/Assignment_03/06 - Khayyam Pascal Trangle.py
@@ -19,21 +19,46 @@ def generate_triangle(number_of_rows):
         triangle.append(khayyam_pascal_triangle(row))
     return triangle
 
-def print_triangle(triangle):
+'''
+def print_centered_triangle(triangle):
     index = 0
+
     for row in triangle:
         spaces = " " * (number_of_rows - index - 1)
         print(spaces + " ".join(map(str, row)))
         index += 1
+'''    
+
+def print_absolutely_centered_triangle(triangle):
+    max_num = max(max(row) for row in triangle)
+    max_len = len(str(max_num))
+
+    last_row = " ".join(f"num:{max_len}" for num in triangle[-1])
+    total_len = len(last_row)
     
+    for row in triangle:
+        # row_string = " ".join(map(str, row))
+        
+
+        row_string = " ".join(f"{num:{max_len}}" for num in row)
+
+        number_of_spaces = (total_len - len(row_string)) // 2
+        number_of_spaces += (total_len - len(row_string)) % 2
+        centered_row = " " * number_of_spaces + row_string
+        print(centered_row)
 
 if __name__ == "__main__":
     while True:
         try:
             number_of_rows = int(input("Enter the number of rows: "))
+            if number_of_rows < 1:
+                print("The number of rows can't be lower than 1.")
+                continue
             break
+        
         except ValueError:
             print("Enter numbers only.")
 
     triangle = generate_triangle(number_of_rows)
-    print_triangle(triangle)
+    # print_triangle(triangle)
+    print_absolutely_centered_triangle(triangle)
